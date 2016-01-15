f9f8a92dbba2eb6f5647d37f0aff93a90859b4ed
Eugene Brown <efbbrown@gmail.com>
2016-01-15 17:16:34 +1100

    Added make_once function

--- R/make.R
+++ R/make.R
@@ -23,3 +23,24 @@ make <- function(cache.file, builder = NULL, verbose = FALSE, max.age=as.difftim
   if (verbose==TRUE) cat("Reading ",cache.file,"\n")
   return (readRDS(file=cache.file))
 }
+
+#' Make a .rds file if it doesn't exist already
+#'
+#' Makes a .rds file according to a builder function
+#'
+#' @param cache.file The file to be made
+#' @param builder The builder function
+#' @param verbose Should I be verbose?
+#' @param ... Additional parameters to be evaluated in the builder function
+#'
+#' @return An R data object
+#'
+#' @examples
+#'
+#' @export
+make_once <- function(cache.file, builder = NULL, verbose = FALSE, ...) {
+  cache.file <- paste0(cache.file,'.rds')
+  if (!file.exists(cache.file)) return (try_make(cache.file, builder, verbose, fatal=TRUE, ...))
+  if (verbose==TRUE) cat("Reading ",cache.file,"\n")
+  return (readRDS(file=cache.file))
+}
