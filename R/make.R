d96aa837b5b54d4b1668f223205a3abff7e66319
Eugene Brown <efbbrown@gmail.com>
2016-03-28 19:45:41 +1100

    Changed make to accept dynamic difftimes instead of just days

--- R/make.R
+++ R/make.R
@@ -16,31 +16,10 @@
 make <- function(cache.file, builder = NULL, verbose = FALSE, max.age=as.difftime(7,units='days'), ...) {
   cache.file <- paste0(cache.file,'.rds')
   if (!file.exists(cache.file)) return (try_make(cache.file, builder, verbose, fatal=TRUE, ...))
-  if (difftime(Sys.time(),file.info(cache.file)$mtime, units='days') > max.age) {
+  if (difftime(Sys.time(),file.info(cache.file)$mtime, units=units(max.age)) > max.age) {
     obj <- try_make(cache.file, builder, verbose)
     if (!'error' %in% class(obj)) return(obj)
   }
-  if (verbose==TRUE) cat("Reading ",cache.file,"\n")
-  return (readRDS(file=cache.file))
-}
-
-#' Make a .rds file if it doesn't exist already
-#'
-#' Makes a .rds file according to a builder function
-#'
-#' @param cache.file The file to be made
-#' @param builder The builder function
-#' @param verbose Should I be verbose?
-#' @param ... Additional parameters to be evaluated in the builder function
-#'
-#' @return An R data object
-#'
-#' @examples
-#'
-#' @export
-make_once <- function(cache.file, builder = NULL, verbose = FALSE, ...) {
-  cache.file <- paste0(cache.file,'.rds')
-  if (!file.exists(cache.file)) return (try_make(cache.file, builder, verbose, fatal=TRUE, ...))
   if (verbose==TRUE) cat("Reading ",cache.file,"\n")
   return (readRDS(file=cache.file))
 }
